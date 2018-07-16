c80797dc34e28ceaa08a8e0ef16f95e5f58ca88f
mehmetucar86@gmail.com <mehmetucar86@gmail.com>
2018-07-16 00:55:24 -0500

    Final TestNG Framework

---
+++ src/test/java/com/cbt/tests/TestBase.java
@@ -0,0 +1,33 @@
+package com.cbt.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.cbt.utilities.ConfigurationReader;
+import com.cbt.utilities.Driver;
+
+public class TestBase {
+	protected WebDriver driver;
+	Actions actions;
+	
+	@BeforeMethod
+	public void setUp() {
+		driver = Driver.getDriver();
+		actions = new Actions(driver);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+		driver.get(ConfigurationReader.getProperty("url"));
+
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		Driver.closeDriver();
+	}
+
+}
